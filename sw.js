ba20a227caa597ef22a7156c6ea43fc565931c36
apssmt287-create <aps.smt287@gmail.com>
2026-06-15 08:06:54 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'pwb-inventory-v1';
+const urlsToCache = [
+  './inventory-count-tag (6).html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
